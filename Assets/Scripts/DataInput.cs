7f3da0632e72459ed66048e00f5704df21e4cfd3
Shave-A <maintoast06@gmail.com>
2026-06-21 18:27:04 +0200

    Changed button sizes and database input works

--- Assets/Scripts/DataInput.cs
+++ Assets/Scripts/DataInput.cs
@@ -1,14 +1,59 @@
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+using TMPro;
+using System.Collections;
+using UnityEngine.SocialPlatforms.Impl;
 
 public class DataInput : MonoBehaviour
 {
     string createUserURL = "http://localhost/infiniteRunner/InsertUsers.php";
-    public void CreateScore(string name)
+
+    public TMP_InputField nameInputField;
+    public Button submitButton;
+
+    void Start()
     {
+        submitButton.onClick.AddListener(OnSubmitClicked);
+    }
+
+    void OnSubmitClicked()
+    {
+        string name = nameInputField.text;
+
+        if (string.IsNullOrEmpty(name))
+        {
+            Debug.Log("Name is empty!");
+            return;
+        }
+
+        StartCoroutine(CreateScore(name));
+    }
+
+    IEnumerator CreateScore(string name)
+    {
+
+        Debug.Log("Sending - Name: " + name + " Score: " + Game.instance.score);
         WWWForm form = new WWWForm();
         form.AddField("username", name);
         form.AddField("score", Game.instance.score);
 
-        WWW www = new WWW(createUserURL, form);
+        using (UnityWebRequest www = UnityWebRequest.Post(createUserURL, form))
+        {
+            yield return www.SendWebRequest();
+
+            Debug.Log("Response code: " + www.responseCode);
+            Debug.Log("Response text: " + www.downloadHandler.text);
+
+            if (www.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("Success: " + www.downloadHandler.text);
+            }
+            else
+            {
+                Debug.LogError("Failed: " + www.error);
+            }
+        }
     }
+
 }
